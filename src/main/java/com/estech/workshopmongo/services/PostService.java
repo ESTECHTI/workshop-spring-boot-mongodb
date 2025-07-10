4358b2b6a42446bd1c127ab2af43c8c8243ca2fd
ESTECHTI <e.s.tech@outlook.com>
2025-07-10 15:07:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/estech/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.estech.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estech.workshopmongo.domain.Post;
+import com.estech.workshopmongo.domain.User;
+import com.estech.workshopmongo.repository.PostRepository;
+import com.estech.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
